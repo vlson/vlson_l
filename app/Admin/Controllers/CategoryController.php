3346b407cb5e9d51341ab9ddcc31c37eb733043d
vlson <lxj370832@163.com>
2020-03-23 17:23:54 +0800

    分类的反显

--- app/Admin/Controllers/CategoryController.php
+++ app/Admin/Controllers/CategoryController.php
@@ -30,12 +30,24 @@ class CategoryController extends AdminController
 
         $grid->filter(function ($filter){
             $filter->scope('trashed', '回收站')->onlyTrashed();
+            $filter->like('cat_name', '分类名称');
         });
+
         $grid->column('id', __('Id'));
         $grid->column('cat_name', __('分类名称'));
-        $grid->column('logo', __('分类LOGO'));
-        $grid->column('parent_id', __('父级分类'));
-        $grid->column('level', __('分类级别'));
+        $grid->column('logo', __('分类LOGO'))->image(env('ALIYUN_OSS_ADDRESS'), 30, 30);
+        $grid->column('parentCategory.cat_name', __('父级分类'))->replace([''=>'无']);
+        $grid->column('level', __('分类级别'))->using([
+            0 => '默认',
+            1 => '一级分类',
+            2 => '二级分类',
+            3 => '三级分类',
+        ], '未知')->dot([
+            0 => 'danger',
+            1 => 'info',
+            2 => 'primary',
+            3 => 'success',
+        ], 'warning');
         $grid->column('created_at', __('创建时间'));
         $grid->column('updated_at', __('更新时间'));
         $grid->column('deleted_at', __('删除时间'));
@@ -67,13 +79,19 @@ class CategoryController extends AdminController
     protected function detail($id)
     {
         $show = new Show(BlogCategoryModel::findOrFail($id));
-        dd('test');
 
         $show->field('id', __('Id'));
         $show->field('cat_name', __('分类名称'));
-        $show->field('logo', __('分类LOGO'));
-        $show->field('parent_id', __('父级分类'));
-        $show->field('level', __('分类级别'));
+        $show->field('logo', __('分类LOGO'))->image(env('ALIYUN_OSS_ADDRESS'), 50, 50);
+        $show->field('parent_id', __('父级分类'))->as(function(){
+            return $this->parentCategory->cat_name;
+        });
+        $show->field('level', __('分类级别'))->using([
+            0 => '默认',
+            1 => '一级分类',
+            2 => '二级分类',
+            3 => '三级分类',
+        ], '未知');
         $show->field('created_at', __('创建时间'));
         $show->field('updated_at', __('更新时间'));
         $show->field('deleted_at', __('删除时间'));
@@ -116,7 +134,7 @@ class CategoryController extends AdminController
         // 分类级别处理
         $parent_level = BlogCategoryModel::withoutTrashed()->where(['id'=>$form_param['parent_id']])->value('level');
         $level = $parent_level + 1;
-        if(!$parent_level){
+        if($parent_level === null){
             $level = 0;
         }
         \request()->offsetSet('level', $level);
--- app/Models/Blog/BlogCategoryModel.php
+++ app/Models/Blog/BlogCategoryModel.php
@@ -12,4 +12,14 @@ class BlogCategoryModel extends BasicModel
     protected $table = 'blog_category';
 
     protected $dates = ['deleted_at'];
+
+
+    public function parentCategory(){
+        return $this->belongsTo(self::class, "parent_id", 'id');
+    }
+
+
+    public function childrenCategory(){
+        return $this->hasMany(self::class, 'id', "parent_id");
+    }
 }
