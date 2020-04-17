9808127877582fd2dd7801284231c0f3aa230441
vlson <lxj370832@163.com>
2020-04-17 17:37:54 +0800

    navbar 增加系统导航样式调整

--- resources/views/admin/search-bar.blade.php
+++ resources/views/admin/search-bar.blade.php
@@ -1,23 +1,40 @@
+<style>
+    .visible-lg-block>li{
+        margin-left: 25px;
+    }
+    .visible-lg-block>li>a{
+        padding: 10px;
+    }
+    .visible-lg-block>li>a>img{
+        width: 31px;
+        height: 31px;
+    }
+</style>
+
+<!-- blog 导航 -->
 <li role="presentation" class="active">
-    <a href="#">
+    <a href="#" target="_blank">
         <img src="https://vlson.oss-cn-beijing.aliyuncs.com/vlson_l/common/blog-icons.png" alt="博客">
     </a>
 </li>
 
+<!-- 微矩阵 导航 -->
 <li role="presentation">
-    <a href="#">
+    <a href="#" target="_blank">
         <img src="https://vlson.oss-cn-beijing.aliyuncs.com/vlson_l/common/wan-icon5.png" alt="微矩阵">
     </a>
 </li>
 
+<!-- 工具 导航 -->
 <li role="presentation">
-    <a href="#">
+    <a href="#" target="_blank">
         <img src="https://vlson.oss-cn-beijing.aliyuncs.com/vlson_l/common/tools-icon.png" alt="工具">
     </a>
 </li>
 
+<!-- 商城 导航 -->
 <li role="presentation">
-    <a href="#">
+    <a href="#" target="_blank">
         <img src="https://vlson.oss-cn-beijing.aliyuncs.com/vlson_l/common/shop-icon.png" alt="商城">
     </a>
 </li>
