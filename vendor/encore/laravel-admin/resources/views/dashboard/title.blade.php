8d768cea9373e6c287c0950f99dd26605a16207a
vlson <lxj370832@163.com>
2020-06-05 15:41:24 +0800

    dashboard title 顺序调整

--- vendor/encore/laravel-admin/resources/views/dashboard/title.blade.php
+++ vendor/encore/laravel-admin/resources/views/dashboard/title.blade.php
@@ -30,7 +30,7 @@
 </div>
 <div class="links">
     <a href="{{config('app.blog_url')}}" target="_blank">博客</a>
-    <a href="{{config('app.matrix_url')}}" target="_blank">商城</a>
-    <a href="{{config('app.tools_url')}}"  target="_blank">工具</a>
     <a href="{{config('app.shop_url')}}"  target="_blank">微矩阵</a>
+    <a href="{{config('app.tools_url')}}"  target="_blank">工具</a>
+    <a href="{{config('app.matrix_url')}}" target="_blank">商城</a>
 </div>
