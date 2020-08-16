006d7ed9114d885da38741832ad38dd153fa192e
vlson <vlson0929@gmail.com>
2020-08-16 18:10:20 +0800

    wechat menu change

--- config/wechat.php
+++ config/wechat.php
@@ -11,28 +11,6 @@ return [
                     "name" => "举个栗子",
                     "sub_button" => [
                         [
-                            "type" => "scancode_waitmsg",
-                            "name" => "扫一扫",
-                            "key" => "scan_code",
-                        ],
-                        [
-                            "type" => "location_select",
-                            "name" => "发送位置",
-                            "key" => "location",
-                        ],
-                    ],
-                ],
-                // 中间菜单栏
-                [
-                    "type" => "pic_photo_or_album",
-                    "name" => "SHOW ME",
-                    "key" => "pic_photo",
-                ],
-                // 右侧菜单栏
-                [
-                    "name" => "微醺",
-                    "sub_button" => [
-                        [
                             "type" => "click",
                             "name" => "最近文章",
                             "key" => "latest_article",
@@ -48,6 +26,28 @@ return [
                             "name" => "主站",
                             "key" => "www",
                             "url" => "http://www.vlson.top",
+                        ],
+                    ],
+                ],
+                // 中间菜单栏
+                [
+                    "type" => "pic_photo_or_album",
+                    "name" => "SHOW ME",
+                    "key" => "pic_photo",
+                ],
+                // 右侧菜单栏
+                [
+                    "name" => "微醺",
+                    "sub_button" => [
+                        [
+                            "type" => "scancode_waitmsg",
+                            "name" => "扫一扫",
+                            "key" => "scan_code",
+                        ],
+                        [
+                            "type" => "location_select",
+                            "name" => "发送位置",
+                            "key" => "location",
                         ],
                     ]
                 ],
