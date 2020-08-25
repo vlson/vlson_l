30ed799c1815c178b46ef76d5a335c19bbdd3ca4
vlson <vlson0929@gmail.com>
2020-08-25 20:59:21 +0800

    声明wechat消息表

--- app/Models/Wechat/WechatMessageModel.php
+++ app/Models/Wechat/WechatMessageModel.php
@@ -3,8 +3,13 @@
 namespace App\Models\Wechat;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class WechatMessageModel extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $table = 'wechat_message';
+
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
 }
